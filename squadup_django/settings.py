10a31e0114ade25fe45a4cd4943e1ee361196b8f
Mysta3 <myshawnestallings@gmail.com>
2020-03-12 12:55:11 -0700

    changed allowed_host back

--- squadup_django/settings.py
+++ squadup_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '3mvmb442xaon=7!$(tmj3kh-0v_icm)7m9#wwerbvge7lg8w(k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'squadup-db.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'squadup-db.herokuapp.com']
 
 
 # Application definition
